f0d5741b0450a652930dfcb865bb9edaf8f10abf
Batuhan Bayazıt <53948634+Batuhan4@users.noreply.github.com>
2025-08-18 23:33:35 +0300

    chore: update contract address

--- lib/contract.ts
+++ lib/contract.ts
@@ -2,7 +2,7 @@ import { getAbiItem, Address } from 'viem';
 import abi from './abi.json';
 
 // TODO: Replace with your deployed contract address
-export const contractAddress: Address = `0x${"B25FC5Fd210058C96bc78593Cb29aC0e8642849e"}`; 
+export const contractAddress: Address = `0x${"153D7Ec125fCf3Ae28a026c128Fc1BAF89a7c48D"}`; 
 
 export const contractConfig = {
   address: contractAddress,
